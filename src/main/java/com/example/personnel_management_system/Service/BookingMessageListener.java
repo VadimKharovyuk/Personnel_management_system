732a0abd4694d6103b3c16a387930564fccd48bf
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-06-18 14:56:07 +0300

    serch add deleye user

--- src/main/java/com/example/personnel_management_system/Service/BookingMessageListener.java
+++ src/main/java/com/example/personnel_management_system/Service/BookingMessageListener.java
@@ -1,9 +1,7 @@
 package com.example.personnel_management_system.Service;
-
 import com.example.personnel_management_system.model.Booking;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
@@ -17,12 +15,12 @@ public class BookingMessageListener {
         // Отправка электронного письма с подтверждением бронирования
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(booking.getEmail());
-        message.setSubject("Booking Confirmation");
+        message.setSubject("Подтверждение бронирования");
         message.setText(
-                "Dear " + booking.getFirstName() + ",\n\n" +
-                        "Thank you for booking with us. We are pleased to confirm your booking on " + booking.getBookingDate() + ".\n\n" +
-                        "If you have any questions, please contact us at support@example.com.\n\n" +
-                        "Best regards,\nYour Company Name"
+                "Уважаемый(ая) " + booking.getFirstName() + ",\n\n" +
+                        "Спасибо за ваше бронирование. Мы рады подтвердить ваше бронирование на " + booking.getBookingDate() + ".\n\n" +
+                        "Если у вас есть вопросы, пожалуйста, свяжитесь с нами по адресу support@example.com.\n\n" +
+                        "С наилучшими пожеланиями,\nВаша компания"
         );
 
         emailService.sendEmail(message);
